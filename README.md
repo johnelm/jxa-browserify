8c96e3f5e500e19fea1182eef68fcf60d5400b60
John Elm <github@johnelm.com>
2017-12-16 13:45:37 -0800

    WIP

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # jxa-browserify
 
 Simple CLI wrapper for browserifying jxa scripts, allowing node modules to be used in jxa scripts.
+
+WIP: this is a work in progress, not ready for general use.
---
+++ index.js
@@ -0,0 +1,10 @@
+var browserify = require('browserify')();
+
+module.exports = {
+  jxaCompile: function( sourceFile ) {
+    // now wrap the file and run osacompile on it, passing osacompile args from the command line
+
+  }
+
+}
+
